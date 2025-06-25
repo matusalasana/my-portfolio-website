92ec4483c9e3e5d091787a5183b0aa5f6ee5acd9
Sana <matusalasana.gmail.com>
2025-06-26 00:24:27 +0300

    toggle mode done!

--- movie-hub/src/components/NavBar.tsx
+++ movie-hub/src/components/NavBar.tsx
@@ -1,11 +1,22 @@
-import { HStack, Image, Text } from "@chakra-ui/react"
+
+import { ClientOnly, HStack, IconButton, Image, Skeleton, Text } from "@chakra-ui/react"
+import { useColorMode } from "./ui/color-mode.tsx"
+import { LuMoon, LuSun } from "react-icons/lu"
+
 import logo from '../assets/movie-svgrepo-com.svg'
 
-function NavBar() {
+const NavBar = () => {
+  const { toggleColorMode, colorMode } = useColorMode()
   return (
+
     <HStack>
-        <Image src={logo} boxSize={'60px'}/>
-        <Text>This is the navbar</Text>
+      <Image src={logo} boxSize={'60px'}/>
+      <Text>This is navbar</Text>
+      <ClientOnly fallback={<Skeleton boxSize="8" />}>
+        <IconButton onClick={toggleColorMode} variant="outline" size="sm">
+          {colorMode === "light" ? <LuSun /> : <LuMoon />}
+        </IconButton>
+      </ClientOnly>
     </HStack>
   )
 }
