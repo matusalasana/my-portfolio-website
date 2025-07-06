df54d6e499da446c2e00bd85d69860f112ac916c
Sana <matusalasana.gmail.com>
2025-07-06 11:48:07 +0300

    sorting movies selection made

--- movie-hub/src/components/SortingMovies.tsx
+++ movie-hub/src/components/SortingMovies.tsx
@@ -1,22 +1,19 @@
 
 
-import useMovies from "./hooks/useMovies";
-
-interface props{
-  onSortChange:()=>void;
-  sortBy: string;
-}
-
-
 
 function SortingMovies() {
 
 
 
   return (
-    <div className="sorting-movies">
-      <h1>Sort Movies</h1>
-    </div>
+
+    <select>
+      <option value="title">A-Z</option>
+      <option value="new">Latest release</option>
+      <option value="old">Oldest release</option>
+      <option value="most-rated">Most rated</option>
+      <option value="least-rated">Least rated</option>
+    </select>
   );
 }
 
