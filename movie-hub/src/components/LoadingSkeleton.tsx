ab1c77b4042086810d07ee2a6a12a90a89caf061
Sana <matusalasana.gmail.com>
2025-07-14 04:19:43 +0300

    movies loading skeleton created

---
+++ movie-hub/src/components/LoadingSkeleton.tsx
@@ -0,0 +1,26 @@
+import { SimpleGrid, Skeleton } from "@chakra-ui/react";
+
+function LoadingSkeleton() {
+  const skeletons= [1,2,3,4,5,6,7,8,9,10,11,12];
+
+  return (
+    <SimpleGrid
+      padding="10px"
+      columns={{ base: 2, md: 3, lg: 5, xl: 6 }}
+      gap="10px"
+    >
+      {skeletons.map((skeleton) => (
+        <Skeleton
+            key={skeleton}
+            borderRadius="10px"
+            width={'100%'}
+            height={'300px'}
+            backgroundColor={'gray.500'}
+        />
+      ))}
+    </SimpleGrid>
+  );
+}
+
+
+export default LoadingSkeleton
