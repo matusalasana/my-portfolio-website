8a8509304ca1cf49acceef5aee4ad069ca7570ca
Sana <matusalasana.gmail.com>
2025-07-15 02:20:56 +0300

    toggle mode updated

--- movie-hub/src/components/ColorModeSwitch.tsx
+++ movie-hub/src/components/ColorModeSwitch.tsx
@@ -1,27 +1,16 @@
-import { HStack, Switch } from "@chakra-ui/react"
+import { HStack, IconButton } from "@chakra-ui/react"
 import { useColorMode } from "./ui/color-mode"
 
-import { useState } from "react"
+import { BiMoon, BiSun } from "react-icons/bi"
 
 
 function ColorModeSwitch (){
     const { toggleColorMode, colorMode } = useColorMode()
-    const [status, setStatus] = useState(colorMode)
-
     return (
         <HStack>
-            <Switch.Root
-                checked={colorMode=='dark'}
-                onChange={toggleColorMode}
-                colorPalette={'teal'}
-                onClick={()=>setStatus(colorMode)}>
-                <Switch.HiddenInput />
-                <Switch.Control>
-                    <Switch.Thumb />
-                </Switch.Control>
-                
-                <Switch.Label key={status}>{colorMode}</Switch.Label>
-            </Switch.Root>
+            <IconButton variant={'ghost'} size={'sm'} onClick={toggleColorMode}>
+                {colorMode=='dark' ? <BiMoon/> : <BiSun/>}
+            </IconButton>
         </HStack>
     )
 }
--- movie-hub/src/components/NavBar.tsx
+++ movie-hub/src/components/NavBar.tsx
@@ -7,7 +7,7 @@ function NavBar() {
   return (
     <HStack justifyContent={'space-between'} padding={'10px'}>
       <HStack>
-        <IoFilmSharp color="teal" size={50}/><Text fontFamily={'sans-serif'} fontSize={'4xl'} fontWeight={'bold'} color={'red.700'}>The Movie Hub</Text></HStack>
+        <IoFilmSharp color="teal" size={50}/><Text fontFamily={'sans-serif'} fontSize={'xx-large'} fontWeight={'bold'} color={'red.700'}>The Movie Hub</Text></HStack>
       <ColorModeSwitch/>
     </HStack>
   )
