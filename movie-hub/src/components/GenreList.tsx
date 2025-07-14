beec62aea503234a63c0aab7f954fe314b1944ea
Sana <matusalasana.gmail.com>
2025-07-14 04:55:59 +0300

    displayed genres using simplegrid

--- movie-hub/src/components/GenreList.tsx
+++ movie-hub/src/components/GenreList.tsx
@@ -1,42 +1,26 @@
-import { Button, Heading, Skeleton,Text } from "@chakra-ui/react";
+import { Box, Button, Heading, SimpleGrid } from "@chakra-ui/react";
 import useGenres from "./hooks/useGenres"
-import { type Genre } from "./hooks/useGenres";
 
-interface Props {
-  onSelectGenre: (genre: Genre) => void;
-  selectedGenre:Genre | null;
-}
+function GenreList() {
 
-
-function GenreList({onSelectGenre,selectedGenre}:Props) {
-
-    const {genres,isLoading,err}=useGenres()
-
-    
-    if (isLoading) return <Skeleton/>
+    const {data:genres}=useGenres()
 
   return (
   <>
+    <Heading fontSize={'2xl'} textAlign={'center'} >Genres</Heading>
     
-    {err && <Text color={'red.500'}>{err}</Text>}
-    <Heading fontSize={'2xl'} marginBottom={3} marginLeft={'10px'} >Genres</Heading>
-    
-    {genres.map(genre=>
-                    <Button
-                      className="genre-button"
-                      backgroundColor={selectedGenre?.id === genre.id ? 'orange.500' : 'normal'}                      
-                      onClick={()=>onSelectGenre(genre)}  
-                      variant={'outline'}
-                      key={genre.id} 
-                      fontSize={'lg'} 
-                      flexDirection={'column'}
-                      justifyContent={'flex-start'}
-                    >
-                      {genre.name}
-                    </Button>
-                  )}
+    <SimpleGrid gapY={'10px'} columns={4}>
+    {genres?.map(genre=>
+      <Box>
+        <Button
+          key={genre.id}
+        >
+          {genre.name}
+        </Button>
+      </Box>
+    )}
 
-
+    </SimpleGrid>
   </>
   )
 }
