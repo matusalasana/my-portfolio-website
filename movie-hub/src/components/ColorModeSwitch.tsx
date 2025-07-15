ba9f7a99a1c2260389c4febfd19762e9745c292f
Sana <matusalasana.gmail.com>
2025-07-15 11:44:26 +0300

    toggle icons interchaged their places

--- movie-hub/src/components/ColorModeSwitch.tsx
+++ movie-hub/src/components/ColorModeSwitch.tsx
@@ -2,15 +2,15 @@ import { HStack, IconButton } from "@chakra-ui/react"
 import { useColorMode } from "./ui/color-mode"
 
 import { BiSun } from "react-icons/bi"
-import { TbMoonStars } from "react-icons/tb"
+import { BsMoonStarsFill } from "react-icons/bs"
 
 
 function ColorModeSwitch (){
     const { toggleColorMode, colorMode } = useColorMode()
     return (
         <HStack>
-            <IconButton cursor={'button'} _hover={{backgroundColor:'blackAlpha.100'}} rounded={'10px'} variant={'plain'} size={'md'} onClick={toggleColorMode}>
-                {colorMode=='dark' ? <TbMoonStars size={''} color="black" /> : <BiSun color="orange"/>}
+            <IconButton cursor={'button'} _hover={{backgroundColor:'blackAlpha.100'}} rounded={'10px'} variant={'plain'} size={'lg'} onClick={toggleColorMode}>
+                {colorMode=='dark' ? <BiSun color="orange"/> : <BsMoonStarsFill color="black" />}
             </IconButton>
         </HStack>
     )
